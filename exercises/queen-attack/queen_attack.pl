245792514a3e13c7005480a71ef61459defa1989
Lucas Polymeris <lucaspolymeris@protonmail.com>
2018-03-01 18:29:49 -0300

    Implementing: queen-attack (#51)
    * Implementing: queen-attack
    
    * correction on config.json
    
    * some corrections
    
    * no choicepoints on declarations
    
    * tests corrections
    
    * Another correction to tests

---
+++ exercises/queen-attack/queen_attack.pl
@@ -0,0 +1,11 @@
+% Please visit http://exercism.io/languages/prolog/installing
+% for instructions on setting up prolog.
+% Visit http://exercism.io/languages/prolog/tests
+% for help running the tests for prolog exercises.
+
+% Replace the goal below with
+% your implementation.
+
+create(false).
+
+attack(false).
