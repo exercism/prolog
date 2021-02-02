39f5b48318ce2f74231090fbf7ab299b8be5781b
Tim Austin <tim@neenjaw.com>
2021-02-01 22:59:08 -0600

    move files to match v3 pattern

--- bin/build.sh
+++ bin/build.sh
@@ -1,21 +1,27 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
 declare -i TEST_RESULT=0
 FAILED_EXERCISES=''
 
-for example_file in exercises/**/*.example
+for example_file in $(find exercises -type f -name '*.example')
 do
-    exercise_dir=$(dirname $example_file)
-    exercise=$(basename $example_file .example)
+    echo "${example_file}"
+    example_dir=$(dirname "${example_file}")
+    exercise_dir=$(dirname "${example_dir}")
+    exercise=$(basename "${example_file}" .example)
     echo '-------------------------------------------------------'
-    echo "Testing $exercise"
-    swipl -f  "$exercise_dir/$exercise.example" -s "$exercise_dir/${exercise}_tests.plt" -g run_tests,halt -t 'halt(1)' -- --all
+    echo "Testing ${exercise}"
+    swipl -f  "${example_dir}/${exercise}.example" -s "${exercise_dir}/${exercise}_tests.plt" -g run_tests,halt -t 'halt(1)' -- --all
     if [ $? -ne 0 ]; then
         TEST_RESULT=1
-        FAILED_EXERCISES+="$exercise\n"
+        FAILED_EXERCISES+="${exercise}\n"
     fi
 done
 
-if [ $TEST_RESULT -ne 0 ]; then
+if [ "${TEST_RESULT}" -ne 0 ]; then
     echo "The following exercises failed"
-    printf $FAILED_EXERCISES
-    exit $TEST_RESULT
+    printf "${FAILED_EXERCISES}"
+    exit "${TEST_RESULT}"
 fi
