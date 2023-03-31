80666bb2622934ab8e10bc1f59e70e7e55cdc40c
Erik Schierboom <erik_schierboom@hotmail.com>
2023-03-31 19:02:07 +0200

    sum-of-multiples: fix tests (#204)
    See https://github.com/exercism/prolog/issues/81

--- exercises/practice/sum-of-multiples/.meta/sum_of_multiples.example.pl
+++ exercises/practice/sum-of-multiples/.meta/sum_of_multiples.example.pl
@@ -1,14 +1,12 @@
-factored([Curr|Factors], Num) :-
-    0 is mod(Num, Curr), !;
-    factored(Factors, Num).
+:- use_module(library(clpfd)).
 
-sum_of_multiples(_, 0, 0) :- !.
 sum_of_multiples(Factors, Limit, Sum) :-
-    Next is Limit - 1,
-    sum_of_multiples(Factors, Next, NextSum),
-    (
-        factored(Factors, Limit),
-        Sum is Limit + NextSum, !;
-        Sum is NextSum
-    ).
-
+    findall(Multiple, multiple(Factors, Limit, Multiple), Multiples),
+    list_to_set(Multiples, UniqueMultiples),
+    sum_list(UniqueMultiples, Sum).
+    
+multiple(Factors, Limit, Multiple) :-
+    Upper #= Limit - 1,
+    between(0, Upper, Multiple),
+    member(Factor, Factors),
+    Multiple rem Factor #= 0.
