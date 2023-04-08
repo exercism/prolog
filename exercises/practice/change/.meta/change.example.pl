61d707bdd9d5d02e9e8e014fe5b236aafe0565e6
Erik Schierboom <erik_schierboom@hotmail.com>
2023-04-08 20:31:11 +0200

    change: speedup

--- exercises/practice/change/.meta/change.example.pl
+++ exercises/practice/change/.meta/change.example.pl
@@ -1,14 +1,9 @@
 :- use_module(library(clpfd)).
 
-fewest_coins(Coins, Target, Change) :-
-    Target #>= 0,
-    length(Coins, N),
-    length(Amounts, N),
-    Amounts ins 0..Target,
-    scalar_product(Coins, Amounts, #=, Target),
-    label(Amounts),
-    coins(Coins, Amounts, Change),
-    !.
+generate_upper_constraint(_, [], []).
+generate_upper_constraint(Max, [Coin|Coins], [Amount|Amounts]) :-
+    Amount in 0..Max,
+    generate_upper_constraint(Coin, Coins, Amounts).
 
 coins([], [], []).
 coins([Coin|Coins], [Amount|Amounts], Change) :-
@@ -16,3 +11,18 @@ coins([Coin|Coins], [Amount|Amounts], Change) :-
     maplist(=(Coin), CoinChange),
     coins(Coins, Amounts, Rest),
     append(CoinChange, Rest, Change).
+
+fewest_coins(Coins, Target, Change) :-
+    Target #>= 0,
+    reverse(Coins, ReversedCoins),
+    [Coin|_] = ReversedCoins,
+    length(Coins, N),
+    length(Amounts, N),
+    Max #= div(Target, Coin),
+    generate_upper_constraint(Max, ReversedCoins, ReversedAmounts),
+    reverse(ReversedAmounts, Amounts),
+    scalar_product(Coins, Amounts, #=, Target),
+    sum(Amounts, #=, TotalCoins),
+    labeling([min(TotalCoins)], Amounts),
+    coins(Coins, Amounts, Change),
+    !.
