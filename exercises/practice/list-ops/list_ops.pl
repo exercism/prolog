2fbd1d55d6245714916a82701fa39769e1859782
Erik Schierboom <erik_schierboom@hotmail.com>
2024-03-12 08:02:55 +0100

    Add `list-ops` exercise

---
+++ exercises/practice/list-ops/list_ops.pl
@@ -0,0 +1,8 @@
+custom_length(List, Length).
+custom_append(L1, L2, Appended).
+custom_concat(List, Concatenated).
+custom_reverse(List, Reversed).
+custom_map(Goal, List, Mapped).
+custom_filter(Goal, List, Filtered).
+custom_foldl(Goal, State, List, Folded).
+custom_foldr(Goal, List, State, Folded).
