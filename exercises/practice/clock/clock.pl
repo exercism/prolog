5667ee0e9c083a2681837df502d896c7e814000f
Erik Schierboom <erik_schierboom@hotmail.com>
2024-03-21 07:50:02 +0100

    Add `clock` exercise

--- exercises/practice/clock/clock.pl
+++ exercises/practice/clock/clock.pl
@@ -1,19 +1,7 @@
-:- use_module(library(clpfd)).
+create_clock(Hours, Minutes, Clock).
 
-create_clock(Hours, Minutes, Clock) :-
-    NormalizedMinutes #= (Hours * 60 + Minutes) mod 1440,
-    Clock = clock(NormalizedMinutes).
+display(Clock, Time).
 
-display(Clock, Time) :-
-    Clock = clock(NormalizedMinutes),
-    Hours #= NormalizedMinutes // 60,
-    Minutes #= NormalizedMinutes mod 60,
-    format(string(Time), "~`0t-d~2+ ~`0t-d~2+", [Hours, Minutes]).
+add_minutes(Clock, Minutes, NewClock).
 
-add_minutes(Clock, Minutes, NewClock) :-
-    Clock = clock(NormalizedMinutes),
-    NewMinutes #= NormalizedMinutes + Minutes,
-    create_clock(0, NewMinutes, NewClock).
-
-subtract_minutes(Clock, Minutes, NewClock) :-
-    add_minutes(Clock, -Minutes, NewClock).
+subtract_minutes(Clock, Minutes, NewClock).
