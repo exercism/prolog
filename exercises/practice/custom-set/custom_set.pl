43752ae89ed63956e7d4720c0b509ace66f3f269
Erik Schierboom <erik_schierboom@hotmail.com>
2024-03-25 14:43:01 +0100

    Add `custom-set` exercise (#405)

---
+++ exercises/practice/custom-set/custom_set.pl
@@ -0,0 +1,18 @@
+create_set(Set).
+create_set(Data, Set).
+
+add(Value, Set, Added).
+
+is_empty(Set).
+
+in_set(Value, Set).
+
+is_subset(Set1, Set2).
+
+is_disjoint(Set1, Set2).
+
+difference(Set1, Set2, Difference).
+
+intersection(Set1, Set2, Intersection).
+
+union(Set1, Set2, Union).
