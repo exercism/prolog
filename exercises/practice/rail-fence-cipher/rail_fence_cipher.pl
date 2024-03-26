37548caa7dc2f51d3a79575c8bb015dd0f8631d7
Erik Schierboom <erik_schierboom@hotmail.com>
2024-03-26 17:44:50 +0100

    Add

--- exercises/practice/rail-fence-cipher/rail_fence_cipher.pl
+++ exercises/practice/rail-fence-cipher/rail_fence_cipher.pl
@@ -1,25 +1,3 @@
-rail_indexes(_, _, [], _, _) :- !.
-rail_indexes(Rails, Length, [Rail|Indexes], Rail, down) :-
-    NewRail is Rail - 1,
-    rail_indexes(Rails, Length, Indexes, NewRail, up), !.
-rail_indexes(Rails, Length, [1|Indexes], 1, up) :-
-    NewRail is Rail + 1,
-    rail_indexes(Rails, Length, Indexes, NewRail, down), !.
-rail_indexes(Rails, Length, [Rail|Indexes], Rail, up) :-
-    NewRail is Rail - 1,
-    rail_indexes(Rails, Length, Indexes, NewRail, up), !.
-rail_indexes(Rails, Length, [Rail|Indexes], Rail, down) :-
-    NewRail is Rail + 1,
-    rail_indexes(Rails, Length, Indexes, NewRail, down), !.
-
-rail_indexes(Rails, Length, Indexes) :-
-    rail_indexes(Rails, Length, Indexes, 1, down).
-
-encode(Plaintext, Rails, Ciphertext) :-
-    string_chars(Plaintext, PlaintextChars),
-    string_chars(Ciphertext, CiphertextChars),
-    same_length(PlaintextChars, CiphertextChars).
+encode(Plaintext, Rails, Ciphertext).
 
 decode(Ciphertext, Rails, Plaintext).
-
-
