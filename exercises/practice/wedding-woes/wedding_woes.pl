9c7ef8daef68dedebef6c6d770c2229f40259b1e
Erik Schierboom <erik_schierboom@hotmail.com>
2024-09-08 12:47:22 +0200

    wedding-woes: add exercise

---
+++ exercises/practice/wedding-woes/wedding_woes.pl
@@ -0,0 +1,60 @@
+male(jack).
+male(oliver).
+male(ali).
+male(james).
+male(simon).
+male(harry).
+female(helen).
+female(sophie).
+female(jess).
+female(lily).
+ 
+parent_of(jack,jess).
+parent_of(jack,lily).
+parent_of(helen, jess).
+parent_of(helen, lily).
+parent_of(oliver,james).
+parent_of(sophie, james).
+parent_of(jess, simon).
+parent_of(ali, simon).
+parent_of(lily, harry).
+parent_of(james, harry).
+ 
+/* Rules */
+father_of(X,Y):- male(X),
+    parent_of(X,Y).
+ 
+mother_of(X,Y):- female(X),
+    parent_of(X,Y).
+ 
+grandfather_of(X,Y):- male(X),
+    parent_of(X,Z),
+    parent_of(Z,Y).
+ 
+grandmother_of(X,Y):- female(X),
+    parent_of(X,Z),
+    parent_of(Z,Y).
+ 
+sister_of(X,Y):- %(X,Y or Y,X)%
+    female(X),
+    father_of(F, Y), father_of(F,X),X \= Y.
+ 
+sister_of(X,Y):- female(X),
+    mother_of(M, Y), mother_of(M,X),X \= Y.
+ 
+aunt_of(X,Y):- female(X),
+    parent_of(Z,Y), sister_of(Z,X),!.
+ 
+brother_of(X,Y):- %(X,Y or Y,X)%
+    male(X),
+    father_of(F, Y), father_of(F,X),X \= Y.
+ 
+brother_of(X,Y):- male(X),
+    mother_of(M, Y), mother_of(M,X),X \= Y.
+ 
+uncle_of(X,Y):-
+    parent_of(Z,Y), brother_of(Z,X).
+
+ancestor_of(X,Y):- parent_of(X,Y).
+ancestor_of(X,Y):- parent_of(X,Z),
+    ancestor_of(Z,Y).
---
+++ exercises/practice/wedding-woes/wedding_woes_tests.plt
@@ -0,0 +1,9 @@
+pending :-
+    current_prolog_flag(argv, ['--all'|_]).
+pending :-
+    write('\nA TEST IS PENDING!\n'),
+    fail.
+
+:- begin_tests(wedding_woes).
+
+:- end_tests(wedding_woes).
