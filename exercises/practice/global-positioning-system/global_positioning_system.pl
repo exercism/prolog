33a2836c6fbc7579b43fd2f8eda65fa41b1fce16
Erik Schierboom <erik_schierboom@hotmail.com>
2024-09-23 15:52:56 +0200

    Add `global-positioning-system` exercise (#444)

---
+++ exercises/practice/global-positioning-system/global_positioning_system.pl
@@ -0,0 +1,16 @@
+:- use_module(library(dcg/basics)).
+
+comma.
+space.
+
+latitude_hemisphere(Hemisphere).
+longitude_hemisphere(Hemisphere).
+
+degrees(Degrees).
+latitude_degrees(Degrees).
+longitude_degrees(Degrees).
+
+latitude(Degrees, Hemisphere).
+longitude(Degrees, Hemisphere).
+
+coordinate(Latitude, LatitudeHemisphere, Longitude, LongitudeHemisphere).
